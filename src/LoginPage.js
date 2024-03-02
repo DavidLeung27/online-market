acda03bc3587237df9fb85acefdd9d1b8003a5b4
David Leung <leungkwinglok@gmail.com>
2024-03-02 09:27:54 +0800

    Login page updated
    all elements required for login without function and style are created

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function LoginPage() {
+  const [phoneLogin, setPhoneLogin] = useState(true);
+  
   return (
-    <div>LoginPage</div>
+    <div>
+      {/* Login Box */}
+      <div>
+        Login
+        <form>
+          <div>
+            <button className="loginMethod" onClick = {() => setPhoneLogin(true)} type="button" id="button_phoneLogin">Phone Number</button>
+            <button className="loginMethod" onClick = {() => setPhoneLogin(false)} type="button" id="button_emailLogin">Email Address</button>
+          </div>
+
+          {/* Show the Phone Login or Email Login */}
+          {(phoneLogin && (
+            <div className="loginInput" id="Login by Phone">
+            <select className="countryCode" name="Country Code" id="Country Code"> 
+              <option value="852">+852</option>
+              <option value="000">+000</option> 
+            </select>
+            <input className="phoneInput" placeholder="Phone Number" id="LoginPhone" type="tel" name="Logine Phone"/>
+          </div>
+          )) || (
+            <div className="loginInput" id="Login by Email">
+            <input className="emailInput" placeholder="Email address" id="LoginEmail" type="email" name="Logine Email"/>
+          </div>
+          )}
+          
+          <div className="loginInput">
+            <input className="passwordInput" placeholder="Password" id="Phone Password" type="password" name="Password"/>
+          </div>
+          <button className="button_login" onClick="notDone" type="button" id="Login">
+            <div className="button_loginText">
+              Login
+            </div>
+          </button>
+        </form>
+      </div>
+
+      {/* SignUp Box */}
+      <div>
+        Sign Up
+        <button className="phone_signup">Sign Up with Phone</button>
+        <button className="google_signup">Sign Up with Google Account</button>
+        <button className="facebook_signup">Sign Up with Facebook Account</button>
+      </div>
+    </div>
+
   )
 }
